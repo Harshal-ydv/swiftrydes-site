91cc69893bee4c7a97ebf7f4a02efc9c8d9899ea
Harshal-ydv <harshaly607@gmail.com>
2026-05-28 14:41:22 +0530

    Change userPoolClientId in config.js
    Updated userPoolClientId in the configuration.

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-south-1_9f8xRsvnt', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'hsmqoo5vhllfju9m831l27v8g', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '69hc4ca4f1fs9qgm3gjuo6if0j', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
