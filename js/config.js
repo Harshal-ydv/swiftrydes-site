746d9d12e3d9790af81908d5e8d7ca251f969025
Harshal-ydv <harshaly607@gmail.com>
2026-05-29 00:57:28 +0530

    Clear sensitive configuration values in config.js
    Updated configuration values to empty strings for security.

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_9f8xRsvnt', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '69hc4ca4f1fs9qgm3gjuo6if0j', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-south-1' // e.g. us-east-2
+        userPoolId: '', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: '' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://j18n5p69jf.execute-api.ap-south-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
